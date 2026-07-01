e55d71fb801fc2d5eae22f87e83845eca7708531
ezekielvincent <vincent@haabaamail.com>
2026-07-02 00:32:46 +0100

    Edited runpod_handler.py

--- backend/runpod_handler.py
+++ backend/runpod_handler.py
@@ -1,6 +1,17 @@
 import runpod
 from handler import handler
 
+print("===================================")
+print("RUNPOD WORKER STARTED SUCCESSFULLY")
+print("===================================")
+
 runpod.serverless.start({
     "handler": handler
 })
+
+# import runpod
+# from handler import handler
+
+# runpod.serverless.start({
+#     "handler": handler
+# })
